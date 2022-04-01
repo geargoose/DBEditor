9106e966becb21bf0f13855e3ba94ba82e4a1ca0
geargoose <kb5qjbgh7q7x@mail.ru>
2022-04-01 17:49:24 +0300

    TODO: fab action

--- app/src/main/java/com/egorzaev/dbeditor/MainActivity.java
+++ app/src/main/java/com/egorzaev/dbeditor/MainActivity.java
@@ -3,12 +3,29 @@ package com.egorzaev.dbeditor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton add_fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_fab = findViewById(R.id.add_fab);
+
+        add_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("MY", "fab click: TODO");
+            }
+        });
+
+
     }
 }
